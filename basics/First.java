88c110b63f8a9dee26d12803bf780336aa6fbe60
siddil100 <119801077+siddil100@users.noreply.github.com>
2023-06-21 12:54:50 +0530

    records

--- basics/First.java
+++
@@ -1,9 +0,0 @@
-public class First{
-	public static void main(String[] args)
-	{
-		System.out.println("Hai zid");
-		int a=10;
-		System.out.println("The value of a is "+a);  //variable devlaration
-		
-	}
-}
